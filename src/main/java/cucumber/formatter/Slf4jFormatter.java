4a3492ac9533b5161d73f17c9bf4bcf4bab1530c
jhberges <jhberges@gmail.com>
2013-10-26 17:50:53 +0200

    Prepare for Freemarker, started to set up Cucumber for testing.

--- src/main/java/cucumber/formatter/Slf4jFormatter.java
+++ src/main/java/cucumber/formatter/Slf4jFormatter.java
@@ -1,5 +1,7 @@
 package cucumber.formatter;
 
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import gherkin.formatter.Formatter;
 import gherkin.formatter.Reporter;
 import gherkin.formatter.model.Background;
@@ -11,115 +13,163 @@ import gherkin.formatter.model.Scenario;
 import gherkin.formatter.model.ScenarioOutline;
 import gherkin.formatter.model.Step;
 
+import java.io.StringWriter;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.logging.Logger;
 
+import org.slf4j.LoggerFactory;
+
+import com.google.common.base.Strings;
 
 public class Slf4jFormatter implements Reporter, Formatter {
-   private static final char PAD = '*';
-   private static final String LINE = Strings.padStart("", 80, PAD);
-   private static final String PFIX = PAD + " ";
-   private final Logger logger = LoggerFactory.getLogger(Slf4jFormatter.class);
-   private final Appendable out;
-   public Slf4jFormatter(final Appendable out) {
-      this.out = out;
-   }
+	private static final char PAD = '*';
+	private static final String LINE = Strings.padStart("", 80, PAD);
+	private static final String PFIX = PAD + " ";
+	private final Logger logger = LoggerFactory.getLogger(Slf4jFormatter.class);
+	private final Appendable out;
+	private final Configuration configuration;
 
-   @Override
-   public void after(final Match match, final Result result) {
-      logger.info("*** AFTER:\n{}{}\n{}{}\n{}", PFIX, match.toMap(), PFIX, result.toMap(), LINE);
-   }
+	public Slf4jFormatter(final Appendable out) {
+		this.out = out;
+		configuration = new Configuration();
+		configuration.setClassForTemplateLoading(Slf4jFormatter.class,
+				"/freemarker");
+	}
 
-   @Override
-   public void background(final Background background) {
-      logger.info("*** BACKGROUND:\n{}{}\n{}", PFIX, background.toMap(), LINE);
-   }
+	@Override
+	public void after(final Match match, final Result result) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("match", match);
+		model.put("result", result);
+		logIfHasOutcome(makeString("after", model));
+	}
 
-   @Override
-   public void before(final Match match, final Result result) {
-      logger.info("*** BEFORE:\n{}{}\n{}{}\n{}", PFIX, match.toMap(), PFIX, result.toMap(), LINE);
-   }
+	@Override
+	public void background(final Background background) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("background", background);
+		logIfHasOutcome(makeString("background", model));
+	}
 
-   @Override
-   public void done() {
-      logger.info("*** DONE. ");
-   }
+	@Override
+	public void before(final Match match, final Result result) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("match", match);
+		model.put("result", result);
+		logIfHasOutcome(makeString("before", model));
+	}
 
-   @Override
-   public void feature(final Feature feature) {
-      logger.info("*** FEATURE:\n{}{}\n{}", PFIX, feature.toMap(), LINE);
-   }
+	@Override
+	public void done() {
+		logIfHasOutcome("*** DONE. ");
+	}
 
-   @Override
-   public void scenario(final Scenario scenario) {
-      logger.info(" *** SCENARIO:\n{}{}\n{}",  PFIX, scenario.toMap(), LINE);
-   }
+	@Override
+	public void feature(final Feature feature) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("feature", feature);
+		logIfHasOutcome(makeString("feature", model));
+	}
 
-   @Override
-   public void uri(final String uri) {
-      logger.info("URI? {}", uri);
-   }
+	@Override
+	public void scenario(final Scenario scenario) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("scenario", scenario);
+		logIfHasOutcome(makeString("scenario", model));
+	}
 
-   @Override
-   public void scenarioOutline(final ScenarioOutline scenarioOutline) {
-      logger.info(" *** SCENARIO-OUTLINE:\n{}{}\n{}",  PFIX, scenarioOutline.toMap(), LINE);
-   }
+	@Override
+	public void uri(final String uri) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("uri", uri);
+		logIfHasOutcome(makeString("uri", model));
+	}
 
-   @Override
-   public void examples(final Examples examples) {
-      logger.info(" *** EXAMPLES:\n{}{}\n{}",  PFIX, examples.toMap(), LINE);
-   }
+	@Override
+	public void scenarioOutline(final ScenarioOutline scenarioOutline) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("scenarioOutline", scenarioOutline);
+		logIfHasOutcome(makeString("scenarioOutline", model));
+	}
 
-   @Override
-   public void step(final Step step) {
-      logger.info(" *** STEP:\n{}{}\n{}",  PFIX, step.toMap(), LINE);
-   }
+	@Override
+	public void examples(final Examples examples) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("examples", examples);
+		logIfHasOutcome(makeString("examples", model));
+	}
 
-   @Override
-   public void eof() {
-      logger.info(" *** EOF?\n{}",  LINE);
-   }
+	@Override
+	public void step(final Step step) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("step", step);
+		logIfHasOutcome(makeString("step", model));
+	}
 
-   @Override
-   public void syntaxError(final String state, final String event,
-         final List<String> legalEvents, final String uri, final Integer line) {
-      logger.info(" *** FUBAR?"
-            + "\n{}{}"
-            + "\n{}{}"
-            + "\n{}{}"
-            + "\n{}{}"
-            + "\n{}{}"
-            + "\n{}",
-            state,
-            event,
-            legalEvents,
-            uri,
-            line,
-            LINE);
-   }
+	@Override
+	public void eof() {
+		logIfHasOutcome(makeString("eof", new HashMap<String, Object>()));
+	}
 
-   @Override
-   public void close() {
-      logger.info(" *** CLOSE?\n{}",  LINE);
-   }
+	@Override
+	public void syntaxError(final String state, final String event,
+			final List<String> legalEvents, final String uri, final Integer line) {
 
-   @Override
-   public void result(final Result result) {
-      logger.info(" *** RESULT?\n{}{}\n{}",  PFIX, result.toMap(), LINE);
-   }
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("state", state);
+		model.put("event", event);
+		model.put("legalEvents", legalEvents);
+		model.put("uri", uri);
+		model.put("line", line);
+		logIfHasOutcome(makeString("syntaxError", model));
+	}
 
-   @Override
-   public void match(final Match match) {
-      logger.info(" *** match?\n{}{}\n{}",  PFIX, match.toMap(), LINE);
-   }
+	@Override
+	public void close() {
+		logIfHasOutcome(makeString("close", new HashMap<String, Object>()));
+	}
 
-   @Override
-   public void embedding(final String mimeType, final byte[] data) {
-      logger.info(" *** EMbEDDING?\n{}{}\n{}{}\n{}",  PFIX, mimeType, PFIX, data, LINE);
-   }
+	@Override
+	public void result(final Result result) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("result", result);
+		logIfHasOutcome(makeString("result", model));
+	}
 
-   @Override
-   public void write(final String text) {
-      logger.info(" *** WRITE?\n{}{}\n{}",  PFIX, text, LINE);
-   }
+	@Override
+	public void match(final Match match) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("match", match);
+		logIfHasOutcome(makeString("match", model));
+	}
+
+	@Override
+	public void embedding(final String mimeType, final byte[] data) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("mimeType", mimeType);
+		model.put("data", data);
+		logIfHasOutcome(makeString("embedding", model));
+	}
+
+	@Override
+	public void write(final String text) {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("write", text);
+		logIfHasOutcome(makeString("write", model));
+	}
+
+	private void logIfHasOutcome(String toLog) {
+		if (null != toLog) {
+			logger.info(toLog);
+		}
+	}
+
+	private String makeString(String templateName, Map<String, Object> model) {
+		Template template = configuration.getTemplate(templateName + ".ftl");
+		StringWriter stringWriter = new StringWriter();
+		template.process(model, stringWriter);
+		return stringWriter.toString();
+	}
 }
---
+++ src/resources/cucumber/formatter/Formatter.feature
@@ -0,0 +1,6 @@
+Feature: Test logger formatter
+
+Scenario:
+Given that a logger has been installed
+When Cucumber executes
+Then stuff is logged
